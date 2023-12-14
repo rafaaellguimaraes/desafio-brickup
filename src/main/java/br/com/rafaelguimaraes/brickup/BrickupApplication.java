18c1e9af0c49ddef5b4891de5157dfd56c3d93be
Rafael Guimaraes <rafaell_guimaraes@outlook.com>
2023-12-14 11:02:21 -0300

    first commit

---
+++ src/main/java/br/com/rafaelguimaraes/brickup/BrickupApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelguimaraes.brickup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrickupApplication.class, args);
+	}
+
+}
