092d51258b2e45cdbf98e70fa5e0c127c1a63c92
Rafael Guimaraes <rafaell_guimaraes@outlook.com>
2023-12-14 11:19:13 -0300

    Configuração da Entidade

---
+++ src/main/java/br/com/rafaelguimaraes/brickup/entity/Task.java
@@ -0,0 +1,43 @@
+package br.com.rafaelguimaraes.brickup.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+	private String description;
+	private Boolean completed;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Boolean getCompleted() {
+		return completed;
+	}
+	public void setCompleted(Boolean completed) {
+		this.completed = completed;
+	}
+}
