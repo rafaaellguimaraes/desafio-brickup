9c7fe4e3488317defe71d2881628624bac03f523
Rafael Guimaraes <rafaell_guimaraes@outlook.com>
2023-12-18 09:11:24 -0300

    Finalizando projeto

--- src/main/java/br/com/rafaelguimaraes/brickup/exception/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package br.com.rafaelguimaraes.brickup.exception;
-
-public class BadRequestException extends RuntimeException {
-	
-	public BadRequestException(String message) {
-		super(message);
-	}
-	
-}
